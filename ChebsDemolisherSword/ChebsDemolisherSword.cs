a2265c7fe27e718c40c7bb8a03eafa8a06afe81a
Joshua Paul Woods <jpw1991@gmail.com>
2023-12-03 11:16:23 +1100

    1.2.1: update jotunn to 2.15.0

--- ChebsDemolisherSword/ChebsDemolisherSword.cs
+++ ChebsDemolisherSword/ChebsDemolisherSword.cs
@@ -15,7 +15,7 @@ namespace ChebsDemolisherSword
     {
         public const string PluginGuid = "com.chebgonaz.chebsdemolishersword";
         public const string PluginName = "ChebsDemolisherSword";
-        public const string PluginVersion = "1.2.0";
+        public const string PluginVersion = "1.2.1";
 
         private Mesh _demolisherSwordMesh;
         private Material _demolisherSwordMaterial;
